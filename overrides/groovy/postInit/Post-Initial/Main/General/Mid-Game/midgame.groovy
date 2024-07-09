0781ac8c2e9a81a67008015b2283fe9e11c29bd1
MSR-Enjoyer <babbibble@gmail.com>
2024-07-09 16:38:13 -0600

    Caballed Golden Apples

--- overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
@@ -5,3 +5,8 @@ replaceRecipeInput(metaitem('processing_array'),
 		[[ore('circuitLuv'), item('advsolars:sunnarium'), ore('circuitLuv')],
 		 [metaitem('robot.arm.iv'), metaitem('hull.iv'), metaitem('robot.arm.iv')],
 		 [ore('circuitLuv'), metaitem('tool.datastick'), ore('circuitLuv')]])
+
+// §bGolden Apple * 1
+mods.gregtech.chemical_reactor.removeByInput(30, [item('minecraft:apple'), item('minecraft:gold_block') * 8], null)
+// §bGolden Apple * 1
+mods.gregtech.large_chemical_reactor.removeByInput(30, [item('minecraft:apple'), item('minecraft:gold_block') * 8], null)
